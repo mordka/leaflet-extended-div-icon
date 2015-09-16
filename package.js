d4fb666fb7e08734bd68d527b456d2600a1f9962
mordka <pawel.smile@gmail.com>
2015-09-17 00:25:16 +0200

    add git url

--- package.js
+++ package.js
@@ -1,17 +1,13 @@
 Package.describe({
   name: 'mordka:leaflet-extended-div-icon',
   version: '1.0.0',
-  // Brief, one-line summary of the package.
   summary: 'L.ExtendedDivIcon - Adds the ability to set the id and style of the element created by L.DivIcon',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/mordka/leaflet-extended-div-icon',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
+  api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']);
   api.use("bevanhunt:leaflet@1.0.3");
   api.addFiles('leaflet-extended-div-icon.js','client');
 });
